bb32eeec29684ef3920ec3278bc48f1c70ab6a9e
Duc <duc.trinh.1501@gmail.com>
2019-02-09 03:09:27 -0500

    Added second activity, can go from search bar to result page

--- mapApp/app/src/main/java/com/example/mapapp/MainActivity.java
+++ mapApp/app/src/main/java/com/example/mapapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mapapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText searchBarEditText = (EditText) findViewById(R.id.searchBarEditText);
+                String keyword = searchBarEditText.getText().toString();
+
+                Intent goToResults = new Intent(getApplicationContext(), SearchResults.class);
+                goToResults.putExtra("haverMap",keyword);
+                startActivity(goToResults);
+            }
+        });
     }
 }
--- mapApp/app/src/main/java/com/example/mapapp/SearchResults.java
+++ mapApp/app/src/main/java/com/example/mapapp/SearchResults.java
@@ -2,6 +2,9 @@ package com.example.mapapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 
 public class SearchResults extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class SearchResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+
+        TextView result = (TextView) findViewById(R.id.resultViewText);
+        String text = getIntent().getExtras().getString("haverMap");
+        result.setText("You searched for " + text);
     }
 }
