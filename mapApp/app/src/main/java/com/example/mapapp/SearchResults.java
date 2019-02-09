71270dfe4b66bdc48e71bb38d12b17c309c4ef2d
Duc <duc.trinh.1501@gmail.com>
2019-02-09 03:53:02 -0500

    Changed title bar color

--- mapApp/app/src/main/java/com/example/mapapp/SearchResults.java
+++ mapApp/app/src/main/java/com/example/mapapp/SearchResults.java
@@ -2,19 +2,33 @@ package com.example.mapapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import java.util.*;
 
 import java.awt.font.TextAttribute;
 
 public class SearchResults extends AppCompatActivity {
+
+    ListView results;
+    ArrayList<String> resultList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
 
-        TextView result = (TextView) findViewById(R.id.resultViewText);
+        TextView announcement = (TextView) findViewById(R.id.announcementViewText);
         String text = getIntent().getExtras().getString("haverMap");
-        result.setText("You searched for " + text);
+        announcement.setText("You searched for " + text);
+
+        results = (ListView) findViewById(R.id.resultsListView);
+        resultList.add("dog");
+        resultList.add("cat");
+        resultList.add("squirrel");
+
+        results.setAdapter(new ArrayAdapter<String>(this, R.layout.listview_layout,
+                resultList));
     }
 }
